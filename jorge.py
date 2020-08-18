d869fb3ec21e4a954820e650fb820bd992d7f5c9
jgarciap186 <jorgegarciap@outlook.com>
2020-08-18 08:13:52 -0600

    segundo codigo

--- jorge.py
+++ jorge.py
@@ -2,4 +2,17 @@ from datetime import timedelta, date
 
 print(timedelta(minutes=100))
 print (date.today())
+print(timedelta(minutes=100))
+print (date.today())
+
+print(timedelta(minutes=100))
+print (date.today())
+
+print(timedelta(minutes=100))
+print (date.today())
+
+print(timedelta(minutes=100))
+print (date.today())
+
+
 
